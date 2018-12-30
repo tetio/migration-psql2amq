26f12e54c4682d4c0c53cd7a5315df9d6836a5aa
Sergi <meuxilus@gmail.com>
2018-12-30 14:55:37 +0100

    some cleaning

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,11 @@ createConnection()
     .then(async connection => {
 
         const repository: Repository<Country> = connection.getRepository(Country)
-
-//const res = await repository.findOne(90)
-
-
         const res = await repository.createQueryBuilder('country')
-//            .leftJoinAndMapMany("country.cities", "city", "cities", "country.country_id = city.country_id")
             .where("country.country_id = 103")
-            //.leftJoinAndSelect("Country", "city", "country.country_id = city.country_id")
+            //.leftJoinAndMapMany("country.cities", "city", "city", "country.country_id = city.country_id")
             .leftJoinAndMapMany("country.cities", "city", "city", "country.country_id = city.country_id")
             .getOne()
-
         //res.map((customer) => publish(Q_CUSTOMERS, JSON.stringify(customer)))
         console.log('#country: ' + JSON.stringify(res))
         connection.close()
