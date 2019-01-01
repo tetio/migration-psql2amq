5c8aadc2a270e10c64751e71700ebd301cf59af0
Sergi <meuxilus@gmail.com>
2019-01-01 19:23:52 +0100

    cleaning up

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createConnection, Repository } from "typeorm";
 import { FFCC_CABECERA } from "./entity/FFCCCabecera.entity";
-import { publish, Q_CUSTOMERS } from './queue.utils';
-import { FFCC_EQUIPOS } from "./entity/FFCCEquipo.entity";
+import { publish, Q_FFCC } from './queue.utils';
 
 // connection settings are in the "ormconfig.json" file
 createConnection()
@@ -9,12 +8,13 @@ createConnection()
 
         const repository = connection.getRepository(FFCC_CABECERA)
         const res = await repository.createQueryBuilder('FFCC_CABECERA')
-            .where("FFCC_CABECERA.ID_EXP = 25117")
+            //.where("FFCC_CABECERA.ID_EXP = 25117")
             .leftJoinAndMapMany("FFCC_CABECERA.equipos", 'FFCC_EQUIPOS', 'FFCC_EQUIPOS', "ffcc_cabecera.id_exp = ffcc_equipos.id_exp")
-            .getOne()
+            .getMany()
             //.getQuery()
-        console.log('#FFCCCabecera: \n' + JSON.stringify(res))
-        //console.log('#FFCCCabecera: \n' +res)
+
+        res.map((ffcc) => publish(Q_FFCC, JSON.stringify(ffcc)))
+        // console.log('#FFCCCabecera: \n' + JSON.stringify(res))
         connection.close()
 
     })
